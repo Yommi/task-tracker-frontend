ba113f159e23bf1c79a36d181c7d677d5bfd6010
Yomi <123415710+Yommi@users.noreply.github.com>
2025-03-15 15:04:10 -0400

    moved deployment to render

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_PREFIX = 'https://mighty-beach-57880-24c5bf220069.herokuapp.com/api/v1';
+const API_PREFIX = 'https://task-tracker-backend-zdqt.onrender.com/api/v1';
 
 export default {
   API_PREFIX,
